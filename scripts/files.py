623051a0ea77a18101a3d7688653e8ab80aadd11
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-08-16 10:55:11 -0700

    Updates to files

--- scripts/files.py
+++ scripts/files.py
@@ -2,6 +2,7 @@
 big5_players_csv = '/Users/hogan/dev/streamlit_proj_new/data/specific-csvs/player_table_big5.csv'
 players_matches_csv = 'data/data_out/final_data/csv_files/results.csv'
 teams_matches_csv = 'data/data_out/final_data/csv_files/players.csv'
+big5_this_year = 'data/data_out/scraped_big5_data/big5_players_data_gwMP_20230815143755.csv'
 
 # files paths
 
