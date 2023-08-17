e20ee9ab87ffe90e6327ed9e8b3b58e3c6bec8d5
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-08-16 20:16:46 -0700

    Updates to paths

--- scripts/files.py
+++ scripts/files.py
@@ -2,8 +2,8 @@
 big5_players_csv = 'data/data_out/scraped_big5_data/player_table_big5.csv'
 players_matches_csv = 'data/data_out/final_data/csv_files/results.csv'
 teams_matches_csv = 'data/data_out/final_data/csv_files/players.csv'
-big5_this_year = 'data/data_out/scraped_big5_data/big5_players_data_gwMP_20230815143755.csv'
-pl_data_gw1 = 'data/data_out/scraped_big5_data/pl_data/big5_player_table_gwMP_20230816133205.csv'
+big5_this_year = 'data/data_out/scraped_big5_data/big5_players_data_gw1.csv'
+pl_data_gw1 = 'data/data_out/scraped_big5_data/pl_data/big5_player_table_gw1.csv'
 
 fdr_csv = 'data/data_out/final_data/csv_files/fdr.csv'
 
