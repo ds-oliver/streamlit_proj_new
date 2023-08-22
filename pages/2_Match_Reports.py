5bbb6d572021b040e89ce9893046ad537c90a3ef
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-08-21 22:07:48 -0700

    Update format

--- pages/2_Match_Reports.py
+++ pages/2_Match_Reports.py
@@ -318,7 +318,7 @@ def main():
 
     top_performers_df[selected_stat] = top_performers_df[selected_stat].apply(lambda x: f"{x:.2f}")
 
-    top_performers_df = top_performers_df.round(2)
+    top_performers_df = top_performers_df.applymap(round_and_format)
 
     # 👈 Display the dataframe 👈
     st.dataframe(top_performers_df[columns_to_show].style.apply(lambda _: styled_top_performers_df, axis=None), use_container_width=True, height=len(top_performers_df) * 40 + 50)
