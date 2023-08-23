b7c510a29239a160b2fcc356ab24f664b9aed9e9
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-08-23 14:27:18 -0700

    Update

--- pages/2_Match_Reports.py
+++ pages/2_Match_Reports.py
@@ -228,7 +228,7 @@ def main():
     matches_col_groups = {key.capitalize(): [col.capitalize() for col in value] for key, value in matches_col_groups.items()}
     # capitalize the contents of the lists in matches_col_groups dictionary
 
-    custom_cmap = create_custom_cmap('#03071e', '#8d99ae', '#9C0207')
+    custom_cmap = create_custom_cmap('#e63946', '#457b9d','#a8dadc', '#f1faee', '#e63946')
 
     print(matches_df.columns.tolist())
 
