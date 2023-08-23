8b4093b0b281e4f4832b45ca72ac2bc66b8197e5
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-08-23 13:05:16 -0700

    Update

--- scripts/functions.py
+++ scripts/functions.py
@@ -1473,14 +1473,14 @@ def get_color(value, cmap):
     text_color = 'white' if brightness < 0.7 else 'black'
     return f'color: {text_color}; background-color: rgba({",".join(map(str, (np.array(rgba_color[:3]) * 255).astype(int)))}, 0.7)'
 
-def get_color_from_palette(value, palette_name='inferno'):
+def get_color_from_palette(value, palette_name='magma'):
     cmap = mpl_cm.get_cmap(palette_name)
     rgba_color = cmap(value)
     color_as_hex = mcolors.to_hex(rgba_color)
     return color_as_hex
 
 def style_dataframe_v2(df, selected_columns):
-    object_cmap = mpl_cm.get_cmap('gnuplot2')
+    object_cmap = mpl_cm.get_cmap('magma')
 
     # Create an empty DataFrame with the same shape as df
     styled_df = pd.DataFrame('', index=df.index, columns=df.columns)
@@ -1507,14 +1507,14 @@ def style_dataframe_v2(df, selected_columns):
         unique_values = df[col].unique().tolist()
 
         if len(unique_values) <= 3:
-            constant_colors = [get_color(i / 2, mpl_cm.get_cmap('inferno')) for i in range(len(unique_values))]
+            constant_colors = [get_color(i / 2, mpl_cm.get_cmap('magma')) for i in range(len(unique_values))]
             color_mapping = {val: color for val, color in zip(unique_values, constant_colors)}
             styled_df[col] = df[col].apply(lambda x: color_mapping[x])
         elif col_dtype in [np.float64, np.int64] and col in selected_columns:
             min_val = df[col].min()
             max_val = df[col].max()
             range_val = max_val - min_val
-            styled_df[col] = df[col].apply(lambda x: get_color((x - min_val) / range_val, mpl_cm.get_cmap('inferno')))
+            styled_df[col] = df[col].apply(lambda x: get_color((x - min_val) / range_val, mpl_cm.get_cmap('magma')))
         elif col_dtype == 'object':
             styled_df[col] = df[col].apply(lambda x: get_color(unique_values.index(x) / len(unique_values), object_cmap))
 
