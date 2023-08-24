0dd5f0f30869476b5066be98b8b4a1975cd696f1
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-08-23 21:44:47 -0700

    Update

--- pages/2_Match_Reports.py
+++ pages/2_Match_Reports.py
@@ -356,7 +356,7 @@ def main():
 
     st.divider()
     st.subheader(f":orange[Top Performers] by :red[{selected_stat} ({selected_aggregation_method})]")
-    st.write(f'**{selected_team}** Players in :green[{percentile_value}th percentile] by **:red[{selected_stat} ({selected_aggregation_method})]**', icon='ℹ')
+    st.write(f'**{selected_team}** players in top {100 - percentile_value}th percentile] by **:red[{selected_stat} ({selected_aggregation_method})]**', icon='ℹ')
 
     # Styling DataFrame
     styled_top_performers_df = style_dataframe_custom(top_performers_df[columns_to_show], columns_to_show, False)
