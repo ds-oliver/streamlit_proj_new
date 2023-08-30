18506692abab7136ac15bc8d4a983dbfe4c5578c
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-08-30 16:06:11 -0700

    Update

--- pages/1_PL_Data.py
+++ pages/1_PL_Data.py
@@ -25,7 +25,7 @@ import io
 import matplotlib.colors as mcolors
 import matplotlib
 from collections import Counter
-# from streamlit_extras.dataframe_explorer import dataframe_explorer
+from streamlit_extras.dataframe_explorer import dataframe_explorer
 from markdownlit import mdlit
 # from streamlit_extras.metric_cards import style_metric_cards
 # from streamlit_extras.stylable_container import stylable_container
