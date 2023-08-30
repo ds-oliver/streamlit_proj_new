ae036f62e3b41cbcc9b5832dbcfd176bc9021825
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-08-29 21:59:39 -0700

    Update

---
+++ pages/6_Upload_Data.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+file = st.file_uploader("Upload a file", type="csv")
+
+if st.button('Get data'):
+    df = pd.read_csv(file)
+    # This display will go away with the user's next action.
+    st.write(df)
+
+if st.button('Save'):
+    # This will always error.
+    df.to_csv('data.csv')
