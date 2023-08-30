0d76f64145936222bcba4ab3d028fed83e92b735
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-08-29 23:08:11 -0700

    Update

--- pages/6_Upload_Data.py
+++ pages/6_Upload_Data.py
@@ -19,29 +19,25 @@ sys.path.append(scripts_path)
 from files import gw4_projections  # Add other imports
 from functions import load_csv  # Add other imports
 
-def filter_by_status_and_position(players, projections):
-    # Filter by status
-    status = st.selectbox('Status', players['Status'].unique())
+def filter_by_status_and_position(players, projections, status):
     filtered_players = players[players['Status'] == status]
-    
     player_list = filtered_players['Player'].unique().tolist()
     projections = projections[projections['Player'].isin(player_list)]
-    
-    # Position based filtering
+
     pos_limit = {'D': 5, 'M': 5, 'F': 3}
     final_list = []
-    
+
     for pos, limit in pos_limit.items():
         pos_players = projections[projections['Pos'] == pos]
         selected_players = pos_players.nlargest(limit, 'ProjFPts')
         final_list.append(selected_players)
-    
+
     result = pd.concat(final_list).reset_index(drop=True)
     result = result.nlargest(10, 'ProjFPts').reset_index(drop=True)
-    
-    # Divide into top 10 outfielders and reserves
+    result.sort_values(by='Pos', key=lambda x: x.map({'D': 1, 'M': 2, 'F': 3}), inplace=True)
+
     top_10 = result.head(10)
-    reserves = result.tail(len(result) - 10)
+    reserves = pd.concat([result, top_10]).drop_duplicates(keep=False).reset_index(drop=True)
     
     return top_10, reserves
 
@@ -51,13 +47,18 @@ def main():
     if uploaded_file:
         players = pd.read_csv(uploaded_file)
         projections = load_csv(gw4_projections)  # Assuming load_csv is a function that reads the csv file into a DataFrame
-    
+
+        unique_statuses = [status for status in players['Status'].unique() if status != 'W (Thu)']
+        status = st.selectbox('Status', unique_statuses)
+
         if st.button('Get data'):
-            top_10, reserves = filter_by_status_and_position(players, projections)
-            st.write("### Top 10 Outfielders")
-            st.write(top_10)
-            st.write("### Reserves")
-            st.write(reserves)
+            st.write("### Top 10 Outfielders and Reserves based on the Selected Status")
+            
+        top_10, reserves = filter_by_status_and_position(players, projections, status)
+        st.write("### Top 10 Outfielders")
+        st.write(top_10)
+        st.write("### Reserves")
+        st.write(reserves)
 
 if __name__ == "__main__":
     main()
