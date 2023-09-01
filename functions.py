27c7ddd7573f45ad40a2c5b7cd67ef53a84f5ca8
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-08-31 19:59:11 -0700

    Updates

--- functions.py
+++ functions.py
@@ -34,7 +34,7 @@ sys.path.append(os.path.abspath(os.path.join('./scripts')))
 from constants import color1, color2, color3, color4, color5, cm
 
 def add_construction():
-    return st.info(""":orange[This app is under construction]""", icon='🏗️')
+    return st.info("""### 🏗️ :orange[This app is under construction]""")
 
 def load_css(file_name="style.css"):
     with open("style.css") as f:
--- pages/4_Upload_Data.py
+++ pages/4_Upload_Data.py
@@ -22,6 +22,9 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+def add_construction():
+    return st.info("""#### 🏗️ :orange[This app is under construction]""")
+
 load_css()
 
 filterwarnings('ignore')
@@ -41,59 +44,160 @@ def local_gif(file_path):
     )
 
 def filter_by_status_and_position(players, projections, status):
+    # Filter players by status
     filtered_players = players[players['Status'] == status]
     player_list = filtered_players['Player'].unique().tolist()
+
+    # Filter projections by the list of players with the selected status
     projections = projections[projections['Player'].isin(player_list)]
 
+    # Positional limits for 'D': Defense, 'M': Midfield, 'F': Forward
     pos_limit = {'D': 5, 'M': 5, 'F': 3}
-    final_list = []
 
-    for pos, limit in pos_limit.items():
-        pos_players = projections[projections['Pos'] == pos]
-        selected_players = pos_players.nlargest(limit, 'ProjFPts')
-        final_list.append(selected_players)
+    # Get the top players by position based on their projected fantasy points
+    final_list = [
+        projections[projections['Pos'] == pos].nlargest(limit, 'ProjFPts')
+        for pos, limit in pos_limit.items()
+    ]
 
+    # Get the top 10 players across all positions
     top_10 = pd.concat(final_list).nlargest(10, 'ProjFPts')
-    top_10.sort_values(by='Pos', key=lambda x: x.map({'D': 1, 'M': 2, 'F': 3}), inplace=True)
-    
-    reserves = projections.drop(top_10.index).reset_index(drop=True)
 
-    return top_10, reserves
+    # Sort by position and reset the index
+    top_10.sort_values(by='Pos', key=lambda x: x.map({'D': 1, 'M': 2, 'F': 3}), inplace=True)
+    top_10.reset_index(drop=True, inplace=True)
+
+    # Get the remaining players as reserves
+    # Here I reset the index before dropping
+    projections.reset_index(drop=True, inplace=True)
+    top_10.reset_index(drop=True, inplace=True)
+
+    # get total projected points for top 10 players 
+    top_10_proj_pts = top_10['ProjFPts'].sum()
+
+    # Using isin() to match players rather than index
+    reserves = (projections[~projections['Player'].isin(top_10['Player'])].reset_index(drop=True)).head(5)
+
+    return top_10, reserves, top_10_proj_pts
+
+# function that will get the average projected points for top 10 players across all managers within the same positional limits
+def get_avg_proj_pts(players, projections, status):
+    
+    average_proj_pts = 0
+
+    for status in players['Status'].unique():
+        top_10, reserves, top_10_proj_pts = filter_by_status_and_position(players, projections, status)
+
+        print(f"Average projected points for {status} top 10 players: {top_10_proj_pts}")
+
+        average_proj_pts += top_10_proj_pts
+
+    return average_proj_pts
+
 
 def main():
+    # Adding construction banner or any other initial setups
     add_construction()
 
     mdlit(
-    """## -> To get your optimal lineup navigate to [Fantrax league home](https://www.fantrax.com/fantasy/league/d41pycnmlj3bmk8y/players;statusOrTeamFilter=ALL;pageNumber=1;positionOrGroup=SOCCER_NON_GOALIE;miscDisplayType=1)...
-        ## Follow the GIF below to populate the Players' data correctly <-
+    """### To get your optimal lineup head to -> @(https://www.fantrax.com/fantasy/league/d41pycnmlj3bmk8y/players;statusOrTeamFilter=ALL;pageNumber=1;positionOrGroup=SOCCER_NON_GOALIE;miscDisplayType=1) & follow the GIF below to populate the Players' data correctly and then download it
         """
         )   
-    
+
+    # Adding vertical space for better UI
     add_vertical_space(2)
 
-    local_gif(fx_gif)
+    # Displaying GIF guide for users
+    local_gif(fx_gif)  # Replace with the actual path to your GIF
     
+    # File upload option for user
     uploaded_file = st.file_uploader("Upload a file", type="csv")
 
     if uploaded_file:
+        # Reading the uploaded CSV file
         players = pd.read_csv(uploaded_file)
-        projections = load_csv(gw4_projections)
+        # Assuming 'gw4_projections' is a file path or URL
+        projections = load_csv(gw4_projections)  # Replace with the actual path
+
+        #rename 'W (Thu)' values in Status column to 'Waivers'
+        players['Status'] = players['Status'].replace({'W (Thu)': 'Waivers'})
+
+        # Extracting unique statuses except 'W (Thu)'
+        unique_statuses = [status for status in players['Status'].unique() if status != 'Waivers']
+
+        # capitalizing the first letter of each unique_statuses
+        unique_statuses = [status.capitalize() for status in unique_statuses]
+
+        # get dataframe of players with status 'W (Thu)'
+        available_players = players[players['Status'] == 'W (Thu)']
+
+        # drop Status column
+        available_players.drop(columns=['Status'], inplace=True)
+
+        # Dropdown for user to select team
+        st.write("### Select your Fantasy team from the dropdown below")
+        status = st.selectbox('List of Teams', unique_statuses)
+
+        # lowercase the status
+        status = status.lower()
+
+        with stylable_container(
+            key="green_button",
+            css_styles="""
+                button {
+                    background-color: #370617;
+                    color: white;
+                    border-radius: 20px;
+                }
+                """,
+        ):
+
         
-        unique_statuses = [status for status in players['Status'].unique() if status != 'W (Thu)']
+            # Button to process the selection
+            if st.button('Get my optimal lineup'):
+                # Displaying top 10 outfielders and reserves based on selected status
+                st.divider()
 
-        # tell the user to select a their team from the dropdown below
-        st.write("### Select your team's status from the dropdown below")
+                col1, col2 = st.columns(2)
 
-        status = st.selectbox('Status', unique_statuses)
+                with col1:
+                    
+                    top_10, reserves, top_10_proj_pts = filter_by_status_and_position(players, projections, status)
+                    st.write(f"### {status} Best XI")
+                    st.dataframe(top_10, use_container_width=True)
+                    st.write("### Reserves")
+                    st.dataframe(reserves, use_container_width=True)
 
-        if st.button('Get data'):
-            st.write("### Top 10 Outfielders and Reserves based on the Selected Status")
-            
-            top_10, reserves = filter_by_status_and_position(players, projections, status)
-            st.write("### Top 10 Outfielders")
-            st.write(top_10)
-            st.write("### Reserves")
-            st.write(reserves)
+                with col2:
+                    
+                    status = 'Waivers'
+                    
+                    # call filter_by_status_and_position function to get top 10 players and reserves
+                    top_10, reserves, top_10_proj_pts = filter_by_status_and_position(players, projections, 'Waivers')
+
+                    st.write(f"### {status} Best XI")
+                    st.dataframe(top_10, use_container_width=True)
+                    st.write("### Reserves")
+                    st.dataframe(reserves, use_container_width=True)
+
+                    col1, col2 = st.columns(2)
+
+                    with col1:
+
+                        # call get_avg_proj_pts function to get the average projected points for top 10 players across all managers within the same positional limits
+                        average_proj_pts = get_avg_proj_pts(players, projections, status)
+
+                        # show the total projected points for top 10 players as style_metric_cards
+                        col1.metric(label="Total Projected FPts", value=top_10_proj_pts)
+
+                        # with col2 we will show the average projected points across all managers top 10 players
+
+                        col2.metric(label="Average Projected FPts of Best XIs across the Division", value=average_proj_pts, delta=top_10_proj_pts - average_proj_pts)
+
+                        style_metric_cards()
+
+
+
 
 if __name__ == "__main__":
     main()
