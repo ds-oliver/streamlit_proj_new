4a2694f5ed5214e35f9d9a4fa38e0fcf39bcd837
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-09-26 20:04:32 -0700

    Updates to Style.CSS

--- pages/3_Match_Events.py
+++ pages/3_Match_Events.py
@@ -75,9 +75,12 @@ def main():
 
     # create a dictionary of player names to positions
     player_to_pos = dict(zip(
-        [player.lower() for player in load_csv(temp_default)['Player']],
-        [pos.lower() for pos in load_csv(temp_default)['Position']]
+        [player for player in load_csv(temp_default)['Player']],
+        [pos for pos in load_csv(temp_default)['Position']]
     ))
+
+    # print the number of players in the dictionary
+    print(f"Number of players in dictionary: {len(player_to_pos)}")
 
     shots_df = load_csv(shots_data)
 
@@ -86,10 +89,13 @@ def main():
 
     players_df = load_csv(matches_data)
 
+    # print len of dataframe
+    print(f"Players dataframe shape: {len(players_df)}")
+
     print(f"Players dataframe shape: {players_df.head()}")
 
     # create position column in shots and players dataframes using player_to_pos dictionary
-    players_df['Position'] = players_df['player'].apply(lambda x: player_to_pos[x])
+    players_df['Position'] = np.where(players_df['player'].isin(player_to_pos.keys()), players_df['player'].map(player_to_pos), 'Unknown')
 
     # Capitalize columns
     shots_df.columns = [col.capitalize() for col in shots_df.columns]
@@ -121,16 +127,26 @@ def main():
 
     selected_columns = matches_col_groups[select_col_group]
 
+    # Aggregation settings
     cols_to_show_as_mean = [col for col in selected_columns if '_pct' in col]
     cols_to_show_as_sum = [col for col in selected_columns if '_pct' not in col]
-    
-    aggregation_functions = {col: 'mean' if col in cols_to_show_as_mean else 'sum' for col in selected_columns}
 
+    aggregation_functions = {col: 'mean' if col in cols_to_show_as_mean else 'sum' for col in selected_columns}
+    aggregation_functions['Player'] = 'count'
+
+    # Perform aggregation and reset index
     team_df = players_df.groupby(['Team', 'Gameweek']).agg(aggregation_functions).reset_index()
+
+    # Rename 'Player' column to 'Player Count'
+    team_df.rename(columns={'Player': 'Player Count'}, inplace=True)
+
+    # Apply the formatting function
     team_df = team_df.applymap(round_and_format)
 
-    columns_to_show_players = ['Player'] + [col for col in selected_columns if col in players_df.columns]
-    columns_to_show_team = ['Team'] + [col for col in selected_columns if col in team_df.columns]
+    # create Subs column that takes count of players minus 11
+
+    columns_to_show_players = [col for col in selected_columns if col in players_df.columns]
+    columns_to_show_team = [col for col in selected_columns if col in team_df.columns]
 
     st.divider()
     col1, col2 = st.columns(2)
@@ -139,7 +155,11 @@ def main():
         home_team = select_match.split(' vs. ')[0]
         st.subheader(f'{home_team}')
         home_team_stats = team_df[team_df['Team'] == home_team]
+        home_team_stats.set_index('Team', inplace=True)
+
         home_players_stats = players_df[players_df['Team'] == home_team]
+        home_players_stats.set_index(['Player', 'Position'], inplace=True)
+
 
         styled_home_players_stats_df = style_tp_dataframe_custom(home_players_stats[columns_to_show_players], columns_to_show_players)
         styled_home_team_df = style_tp_dataframe_custom(home_team_stats[columns_to_show_team], columns_to_show_team)
@@ -151,7 +171,10 @@ def main():
         away_team = select_match.split(' vs. ')[1]
         st.subheader(f'{away_team}')
         away_team_stats = team_df[team_df['Team'] == away_team]
+        away_team_stats.set_index('Team', inplace=True)
+
         away_players_stats = players_df[players_df['Team'] == away_team]
+        away_players_stats.set_index(['Player', 'Position'], inplace=True)
 
         styled_away_players_stats_df = style_tp_dataframe_custom(away_players_stats[columns_to_show_players], columns_to_show_players)
         styled_away_team_df = style_tp_dataframe_custom(away_team_stats[columns_to_show_team], columns_to_show_team)
