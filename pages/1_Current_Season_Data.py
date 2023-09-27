2fda6b65a24c3e381ae19fe8032dfd1d0d90af0b
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-09-27 15:36:15 -0700

    Updates

--- pages/1_Current_Season_Data.py
+++ pages/1_Current_Season_Data.py
@@ -282,7 +282,7 @@ def get_color(value, cmap):
     print(f"Value: {value}, RGBA Color: {rgba_color}, Brightness: {brightness}")
     
     # Adjust the brightness threshold
-    text_color = 'white' if brightness < 0.5 else 'black'
+    text_color = 'white' if brightness < 0.8 else 'black'
     
     return f'color: {text_color}; background-color: rgba({",".join(map(str, (np.array(rgba_color[:3]) * 255).astype(int)))}, 0.5)'
 
