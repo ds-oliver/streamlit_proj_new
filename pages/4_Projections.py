39773c70ace5c2db318bef40bee338bccb7ee4aa
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-10-11 12:17:02 -0700

    Updates

--- pages/4_Projections.py
+++ pages/4_Projections.py
@@ -263,7 +263,7 @@ def main():
 
 
 
-            players['Status'] = players['Status'].str.replace(r'^W \(', 'Waivers', regex=True)
+            players['Status'] = players['Status'].str.replace(r'^W \(*', 'Waivers', regex=True)
             unique_statuses = players['Status'].unique()
             available_players = players[players['Status'].isin(['Waivers', 'FA'])]
 
