447a1b5b5e17f48c5243561ac4f2cf606bc873c1
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-10-11 15:07:35 -0700

    Updates

--- pages/4_Projections.py
+++ pages/4_Projections.py
@@ -355,7 +355,7 @@ def main():
                     st.dataframe(reserves)
 
                 with col2:
-                    available_players = pd.merge(available_players, projections[['Player', 'ProjGS', 'ROS Ranks']], on='Player', how='left')
+                    available_players = pd.merge(available_players, projections[['Player', 'ProjGS']], on='Player', how='left')
                     top_10_waivers, reserves_waivers = filter_available_players_by_projgs(
                         available_players, projections, ['Waivers', 'FA'], 1 if st.session_state.only_starters else None
                     )
