1a37872d59194f677d2c1e377e24da489b7caab1
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2023-11-20 18:43:06 -0800

    W

--- scripts/files.py
+++ scripts/files.py
@@ -26,8 +26,8 @@ temp_gw1_fantrax_default = 'data/specific-csvs/Fantrax-Players-DraftPL Super-Lea
 # match reports 
 # data/data_out/scraped_big5_data/pl_data/all_shots_all_20230820.csv data/data_out/scraped_big5_data/pl_data/full_season_matchreports_20230820.csv
 
-shots_data = 'data/data_out/scraped_big5_data/pl_data/all_shots_all_202310.csv'
-matches_data = 'data/data_out/scraped_big5_data/pl_data/full_season_matchreports_202310.csv'
+shots_data = 'data/data_out/scraped_big5_data/pl_data/all_shots_all_20231115.csv'
+matches_data = 'data/data_out/scraped_big5_data/pl_data/full_season_matchreports_20231115.csv'
 
 # files paths
 data_out = 'data/data_out'
